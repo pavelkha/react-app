1e011dff39d041115635f410f40c1cfd3ca21cac
Pavel <pkha0430@gmail.com>
2022-06-07 18:07:52 +0300

    add language switch to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 
 const Header = () => {
+  const [language, setLanguage] = useState('en');
+  const languagesArray = ['en', 'ru'];
+
+  const changeLanguage = event => {
+    const buttonLanguage = event.target.innerText.toLowerCase();
+    if (buttonLanguage !== language) {
+      setLanguage(buttonLanguage);
+    };
+  };
+  
+  const renderLanguageButtons = () => {
+    return (
+      languagesArray.map((currentLang, index) => {
+        const className = currentLang === language ? 
+        'header-language-button header-active-language-button' : 
+        'header-language-button';
+        return (
+          <li key={index}>
+            <button onClick={changeLanguage} className={className}>
+              {currentLang}
+            </button>
+          </li>
+        );
+      })
+    );
+  };
+
   return (
     <header className='header-wrapper'>
-      <ul className='header-pages-list'>
-        <li>Home</li>
-        <li>page 1</li>
-        <li>page 2</li>
-        <li>page 3</li>
-      </ul>
+      <div className='header-flex-container'>
+        <ul className='header-pages-list'>
+          <li>Home</li>
+          <li>page 1</li>
+          <li>page 2</li>
+          <li>page 3</li>
+        </ul>
+        <ul className='header-languages-list'>
+          {renderLanguageButtons()}
+        </ul>
+      </div>
       <h1 className='header-title'>React App</h1>
     </header>
   );

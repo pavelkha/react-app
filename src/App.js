f9e5347b20a5014734749cf6b53b0040468184c0
Pavel <pkha0430@gmail.com>
2022-06-12 18:55:48 +0300

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Main />
+      <Footer />
     </BrowserRouter>
   );
 };
